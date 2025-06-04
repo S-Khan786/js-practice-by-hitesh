139e20d829b64aeef34c53b8c132fbfeb761e2f7
S-Khan786 <osahilkhan654@gmail.com>
2025-06-04 20:39:14 +0530

    learned data_type typeof

---
+++ javascript_concept/2.data_types.js
@@ -0,0 +1,13 @@
+/* Data Types */
+
+// number => 2 to power 53
+// String => ""
+// bigint
+// undefined
+// boolean => true / false
+// null => standalone value
+// symbol
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
