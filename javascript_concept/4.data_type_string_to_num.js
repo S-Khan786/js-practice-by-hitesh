ae606414bdda96b6ddfd6c01c382a1a21f3392b0
S-Khan786 <osahilkhan654@gmail.com>
2025-06-05 17:12:15 +0530

    learned data_type_string_to_num

---
+++ javascript_concept/4.data_type_string_to_num.js
@@ -0,0 +1,33 @@
+/* Why string to number conversion is confusing */
+/* Operations */
+
+
+// let value = 3;
+// let negValue = -value;
+
+// console.log(negValue); // -3
+
+// console.log(2+2);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "Hello";
+// let str2 = " Sahil";
+
+// console.log(str1 + str2);
+
+
+// console.log("1" + 2); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + "2"); // 12
+// console.log(1 + 2 + "2"); // 32
+
+console.log(true); // print true
+console.log(+true); // Possible print -> 1
+// console.log(true+); // Not Possible
+
+
+// Read About Prefix and Postfix Operator 
