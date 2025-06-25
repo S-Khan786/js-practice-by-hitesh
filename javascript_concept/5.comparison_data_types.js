79add4db6289e11d6e9cfa656fe5b418613136d5
S-Khan786 <osahilkhan654@gmail.com>
2025-06-25 20:53:38 +0530

    learned data_types summary

---
+++ javascript_concept/5.comparison_data_types.js
@@ -0,0 +1,8 @@
+/* Comparison of datatypes in javascript */
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
---
+++ javascript_concept/data_types_summary.js
@@ -0,0 +1,23 @@
+// Primitive
+// 7 types -> String, Number, null, undefined, boolean, BigInt, Symbol -> Call by Value
+
+
+// Non - Primitive -> Reference type
+// (), {}, [] -> Array, Objects, Functions
+
+const heroes = ["shaktiman", 'krish', 'doga'];
+let myobj = {
+    name: "hitesh",
+    age: 22,
+}
+
+function fun1() {
+
+}
+const fun1 = () => {
+
+}
+
+const myFun = function() {
+    console.log("Hello World");
+}
