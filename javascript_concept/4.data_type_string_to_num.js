b25e91d6460137c364794979ec773272f7ecde75
S-Khan786 <osahilkhan654@gmail.com>
2025-07-08 10:06:16 +0530

    some changes

--- javascript_concept/4.data_type_string_to_num.js
+++ javascript_concept/4.data_type_string_to_num.js
@@ -24,6 +24,7 @@
 // console.log("1" + 2 + 2); // 122
 // console.log(1 + "2"); // 12
 // console.log(1 + 2 + "2"); // 32
+console.log(1 + "2" + "2"); // 122
 
 console.log(true); // print true
 console.log(+true); // Possible print -> 1
